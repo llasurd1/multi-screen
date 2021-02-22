318b4f5c69f28106ccee1cffdde1245e8d1fb831
llasurd1 <llasurdo@gmail.com>
2021-02-22 10:11:50 -0500

    first commit

--- app/src/main/java/com/example/multi_screen/MainActivity.java
+++ app/src/main/java/com/example/multi_screen/MainActivity.java
@@ -2,13 +2,22 @@ package com.example.multi_screen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button act_button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        act_button = findViewById(R.id.act_button);
+    }
+    public void switch_activites(View view) {
+        Intent act_action =  new Intent(this, Activity2.class);
+        startActivity(act_action);
     }
 }
