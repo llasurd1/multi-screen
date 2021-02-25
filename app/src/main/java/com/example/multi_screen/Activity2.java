92080da6b55a0f5aaadc9ce37550e7e17d610db6
llasurd1 <llasurdo@gmail.com>
2021-02-24 20:00:53 -0500

    made button work both ways

--- app/src/main/java/com/example/multi_screen/Activity2.java
+++ app/src/main/java/com/example/multi_screen/Activity2.java
@@ -2,13 +2,22 @@ package com.example.multi_screen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Activity2 extends AppCompatActivity {
 
+    Button act2_button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
+        act2_button = findViewById(R.id.act2_button);
+    }
+    public void switch2_activites(View view) {
+        Intent act2_action =  new Intent(this, MainActivity.class);
+        startActivity(act2_action);
     }
 }
